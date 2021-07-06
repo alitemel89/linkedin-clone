c4cf54403caf6d7f48cf507c90e4533467949073
alitemel89 <alitemel89.ce@gmail.com>
2021-07-06 22:38:56 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
